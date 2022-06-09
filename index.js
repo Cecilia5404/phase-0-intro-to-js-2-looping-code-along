6634b19d8fb24f122af4ca5f21ce698616e0312f
Cecil-Anyega <ceciliaanyega@gmail.com>
2022-06-09 16:51:19 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+  const writeCards = (name) => {
+    let namesArr = [];
+    for(let i = 0; i < name.length; i++) {
+       namesArr.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return namesArr;
+ }
+ 
+ const countDown = (num) => {
+    for(let i = num; i >= 0; i--) {
+       console.log(i);
+    }
+ }   
+    
+
+
+
+
+
+  
+
+
